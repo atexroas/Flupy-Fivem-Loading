86311216f0deafe2d2a20c3c83efefb378bf4919
atexroas <105064369+atexroas@users.noreply.github.com>
2022-10-12 04:43:20 +0200

    Add files via upload

---
+++ Flupy/__resource.lua
@@ -0,0 +1,11 @@
+files {
+    'load.html',
+    'style.css',
+    'logo.png',
+    'fond.png',
+    'music/music.mp3',
+}
+
+loadscreen 'load.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
